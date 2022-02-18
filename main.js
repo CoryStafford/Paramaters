61e9d95406ca751b0e80072431432c9e9016f206
CoryStafford <corystaffordmusic@gmail.com>
2022-02-18 15:20:51 -0600

    parameters exercise

---
+++ main.js
@@ -0,0 +1,18 @@
+const quarterValue = (num1) => {
+    const newNum = num1 / 4 
+    return newNum
+}
+const functionResult = quarterValue(100)
+// Invoke the function and store the return value
+
+
+// Log the return value
+console.log(functionResult)
+
+/*Define a parameter that will hold the value of the original number.
+Divide that number by 4
+Return the quartered number
+Store the returned number in a variable
+Plug that variable into the parenthesis for the console.log()
+When you run the code, you should see the answer in the console.*/
+
